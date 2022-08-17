3ef101282d86cca6cf92c82b83ff1d8853e8668b
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-08-17 18:55:51 -0400

    update material styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,42 @@ import {
 import LaunchList from './components/launch-list';
 import DetailView from './components/detail-view';
 import './App.css';
+import { Container, Box, Paper } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import darkTheme from './theme';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <LaunchList />
-          </Route>
-          <Route exact path="/:id">
-            <DetailView />
-          </Route>
-        </Switch>
-      </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+        <Container maxWidth="lg" >
+        <Paper elevation={3} square sx={{mb: "1rem", px: "1rem"}} >
+          <Box
+            display="flex"
+            justifyContent="start"
+            alignItems="center"
+            height="5rem"
+            maxWidth="lg"
+            >
+              <h3> SpaceX Launches</h3>
+          </Box>
+        </Paper>
 
-    </Router>
+        <Box>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <LaunchList />
+              </Route>
+              <Route exact path="/:id">
+                <DetailView />
+              </Route>
+            </Switch>
+          </Router>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- src/components/launch-list.tsx
+++ src/components/launch-list.tsx
@@ -7,29 +7,31 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { useQuery, gql } from '@apollo/client';
-import { PastLaunchesListDocument, PastLaunchesListQuery, PastLaunchesListQueryVariables } from "../graphql/generated";
+import {
+  PastLaunchesListDocument,
+  PastLaunchesListQuery,
+  PastLaunchesListQueryVariables
+} from "../graphql/generated";
 
 const LaunchList = () => {
 
   const { data, loading, error } =
-    useQuery<PastLaunchesListQuery, PastLaunchesListQueryVariables>(PastLaunchesListDocument, {
-      variables: {
-        limit: 2
-      }
-    })
+    useQuery<PastLaunchesListQuery, PastLaunchesListQueryVariables>(
+      PastLaunchesListDocument, { variables: { limit: 2 } }
+    )
   
   console.log('data', data)
   let cards;
 
   if (data?.launchesPast) {
-    cards = data?.launchesPast.map((launch, idx) => {
-      return (
-        <Grid item xs={4}>
-          <Card sx={{ maxWidth: 500 }} key={`launch-${idx}`}>
+    cards = data?.launchesPast.map((launch, idx) => 
+        <Grid item xs={4} key={`launch-${idx}`}>
+          <Card sx={{ maxWidth: 500 }}>
             <CardMedia
               component="img"
               height="150"
-              image={launch?.links?.mission_patch_small || undefined}
+              image={ launch?.links?.mission_patch_small ||
+                "https://via.placeholder.com/300x125.png?text=Mission+Patch" }
               alt={`${launch?.mission_name}-img`}
             />
             <CardContent>
@@ -45,8 +47,7 @@ const LaunchList = () => {
             </CardActions>
           </Card>
         </Grid>
-      )
-    })
+    )
   }
 
   return (
